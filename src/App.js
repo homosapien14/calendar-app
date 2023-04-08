0ea285b99c37ba418ddc751cf790d7f756da8514
homosapien14 <spma259@gmail.com>
2023-04-08 21:54:15 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
 import  {EnrollPage} from './pages/enrollPage';
 import {SchedulePage} from './pages/schedulePage';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 
 export default function App() {
-  let [data, setData] = useState([]);
+  // let [data, setData] = useState([]);
   return (
     <BrowserRouter>
-      <Link to="/Schedule" state={{data:data}} className="link">
+      <Link to="/Schedule" state={{data:[]}} className="link">
         
       </Link>
       <div className='App'  >
